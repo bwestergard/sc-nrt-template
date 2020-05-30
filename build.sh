141467d758c92028440e4f9342c2082d4ff2d898
Bjorn Westergard <bjornw@gmail.com>
2020-05-30 17:50:52 -0400

    stranded changes
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+export SC_SYNTHDEF_PATH=./build;
+
 mkdir -p build;
 rm build/*;
 sclang -d . composition/writeSynthDefs.sc;
-node composition/generateScore.js
+node composition/generateScore.js;
 scsynth -N ./build/javascriptScore.osc _ ./build/output.aiff 44100 AIFF int16
